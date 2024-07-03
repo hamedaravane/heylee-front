a5730b650367a88ee94fc8ee8c34b85eb1c4420a
hamed <h.arghavan@ompfinex.com>
2024-07-03 18:58:05 +0330

    feat(badge): add badge and cart icon

--- src/app/main/component/main/main.component.ts
+++ src/app/main/component/main/main.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { RouterOutlet } from '@angular/router';
 import { NgOptimizedImage } from '@angular/common';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
 
 @Component({
   selector: 'main',
   standalone: true,
-  imports: [NzLayoutModule, RouterOutlet, NgOptimizedImage],
+  imports: [NzLayoutModule, RouterOutlet, NgOptimizedImage, NzGridModule, NzBadgeModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
