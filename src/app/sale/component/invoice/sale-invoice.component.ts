68e6130c67e7bdc823fbd2fd07841981ffebb816
hamed <h.arghavan@ompfinex.com>
2024-07-06 20:05:36 +0330

    feat(sale): sale invoice component

--- src/app/sale/component/invoice/sale-invoice.component.ts
+++ src/app/sale/component/invoice/sale-invoice.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'sale-invoice',
+  imports: [NzButtonModule, ReactiveFormsModule],
   standalone: true,
   templateUrl: './sale-invoice.component.html',
 })
 export class SaleInvoiceComponent {
+  private formBuilder = inject(FormBuilder);
+  orderForm = this.formBuilder.group({
+    saleItems: this.formBuilder.array([]),
+    customer: this.formBuilder.group({
+      name: '',
+      address: '',
+      phone: '',
+      city: '',
+      postalCode: '',
+      description: ''
+    })
+  })
 
+  onSubmit() {
+    console.log(this.orderForm.value);
+  }
 }
