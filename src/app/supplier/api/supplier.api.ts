d7e6509d66d1d1068e1b728d19f76ef9b517516d
Hamed Arghavan <hamedaravane@gmail.com>
2024-07-24 02:25:26 +0330

    fix(suppliers): add supplier.api.ts

---
+++ src/app/supplier/api/supplier.api.ts
@@ -0,0 +1,23 @@
+import {inject, Injectable} from "@angular/core";
+import {SupplierFacade} from "@supplier/data-access/supplier.facade";
+import {Observable} from "rxjs";
+import {IndexResponse} from "@shared/entity/server-response.entity";
+import {Supplier} from "@supplier/entity/supplier.entity";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupplierApi {
+  private readonly supplierFacade = inject(SupplierFacade);
+  private isFetched = false;
+
+  get suppliers$(): Observable<IndexResponse<Supplier>> {
+    if (!this.isFetched) {
+      this.supplierFacade.loadSuppliers().then(() => {
+        this.isFetched = true;
+      });
+      return this.supplierFacade.suppliersIndex$;
+    }
+    return this.supplierFacade.suppliersIndex$;
+  }
+}
