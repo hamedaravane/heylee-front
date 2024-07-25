deaab3ab28bbbc639705ab27f70104f1be65cfc7
Hamed Arghavan <hamedaravane@gmail.com>
2024-07-26 01:09:51 +0330

    fix(sale invoice-list): sale invoice lists more info modal

--- src/app/sale/component/invoice-list/invoice-list.component.ts
+++ src/app/sale/component/invoice-list/invoice-list.component.ts
@@ -1,27 +1,23 @@
 import {Component, DestroyRef, inject, OnInit} from '@angular/core';
 import {SaleFacade} from '@sale/data-access/sale.facade';
-import {AsyncPipe, CurrencyPipe, DatePipe, DecimalPipe, NgIf} from '@angular/common';
+import {AsyncPipe, CurrencyPipe, DecimalPipe, NgIf} from '@angular/common';
 import {NzEmptyModule} from 'ng-zorro-antd/empty';
 import {NzSkeletonModule} from 'ng-zorro-antd/skeleton';
 import {FormsModule} from '@angular/forms';
-import {NzDividerModule} from 'ng-zorro-antd/divider';
 import {NzPaginationModule} from 'ng-zorro-antd/pagination';
-import {RouterLink} from '@angular/router';
 import {PageContainerComponent} from "@shared/component/page-container/page-container.component";
 import {CardContainerComponent} from "@shared/component/card-container/card-container.component";
 import {PersianDatePipe} from "@sale/pipe/persian-date.pipe";
 import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 import {SaleInvoice} from "@sale/entity/invoice.entity";
-import {NzCollapseModule} from "ng-zorro-antd/collapse";
 import {CurrencyComponent} from "@shared/component/currency-wrapper/currency.component";
-import {NzDrawerModule} from "ng-zorro-antd/drawer";
 import {NzButtonModule} from "ng-zorro-antd/button";
 import {NzModalModule} from "ng-zorro-antd/modal";
 
 @Component({
   selector: 'invoice-list',
   standalone: true,
-  imports: [NzDrawerModule, NzModalModule, NzButtonModule, NzPaginationModule, NzCollapseModule, AsyncPipe, NzEmptyModule, DatePipe, NzDividerModule, CurrencyPipe, NzSkeletonModule, DecimalPipe, FormsModule, RouterLink, PageContainerComponent, CardContainerComponent, PersianDatePipe, CurrencyComponent, NgIf],
+  imports: [NzModalModule, NzButtonModule, NzPaginationModule, AsyncPipe, NzEmptyModule, CurrencyPipe, NzSkeletonModule, DecimalPipe, FormsModule, PageContainerComponent, CardContainerComponent, PersianDatePipe, CurrencyComponent, NgIf],
   templateUrl: './invoice-list.component.html',
 })
 export class InvoiceListComponent implements OnInit {
@@ -29,7 +25,6 @@ export class InvoiceListComponent implements OnInit {
   private readonly destroyRef = inject(DestroyRef);
   invoiceData: SaleInvoice[] = [];
   loadingState = false;
-  moreInfo = false;
 
   ngOnInit() {
     this.saleFacade.loadInvoices().then();
