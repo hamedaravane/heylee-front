d36d51ae4b6602742faaa4ce6248657e3d490552
Hamed Arghavan <hamedaravane@gmail.com>
2024-08-01 02:14:53 +0330

    fix(size & colors): fix labels.constant.ts

--- src/app/product/constant/labels.constant.ts
+++ src/app/product/constant/labels.constant.ts
@@ -4,23 +4,24 @@ interface IdLabel {
 }
 
 export const sizeLabels: IdLabel[] = [
-  {id: 1, label: "S"},
-  {id: 2, label: "M"},
-  {id: 3, label: "L"},
-  {id: 4, label: "XL"},
-  {id: 5, label: "XXL"},
-  {id: 6, label: "M-XL"},
-  {id: 7, label: "S-L"},
-  {id: 8, label: "70"},
-  {id: 9, label: "75"},
-  {id: 10, label: "80"},
-  {id: 11, label: "85"},
-  {id: 12, label: "90"},
-  {id: 13, label: "75 - 90"}
+  {id: 1, label: "S-L"},
+  {id: 2, label: "M-XL"},
+  {id: 3, label: "75"},
+  {id: 4, label: "80"},
+  {id: 5, label: "85"},
+  {id: 6, label: "90"},
+  {id: 7, label: "FREE"},
+  {id: 8, label: "95"},
+  {id: 9, label: "XL"},
+  {id: 10, label: "L"},
+  {id: 12, label: "70"},
+  {id: 13, label: "M"},
+  {id: 14, label: "S"}
 ]
 
+
 export const colorLabels: IdLabel[] = [
-  {id: 1, label: "مشکی"},
+  {id: 1, label: "سیاه"},
   {id: 2, label: "سفید"},
   {id: 3, label: "خاکستری"},
   {id: 4, label: "آبی"},
@@ -33,17 +34,18 @@ export const colorLabels: IdLabel[] = [
   {id: 11, label: "یاسی"},
   {id: 12, label: "خاکی"},
   {id: 13, label: "فیروزه‌ای"},
-  {id: 14, label: "بادمجونی"},
-  {id: 15, label: "سبز کله غازی"},
-  {id: 16, label: "طوسی"},
-  {id: 17, label: "سبز فسفری"},
-  {id: 18, label: "بنفش"},
-  {id: 19, label: "سبز"},
-  {id: 20, label: "سبز صدری"},
-  {id: 21, label: "آجری"},
-  {id: 22, label: "خردلی"},
-  {id: 23, label: "آبی کاربنی"},
-  {id: 24, label: "سبز یشمی"},
-  {id: 25, label: "زیتونی"},
-  {id: 26, label: "قهوه‌ای"}
+  {id: 14, label: "مشکی"},
+  {id: 15, label: "طوسی"},
+  {id: 16, label: "شیری"},
+  {id: 17, label: "سبز کلاغی"},
+  {id: 18, label: "بادمجونی"},
+  {id: 19, label: "سبز فسفری"},
+  {id: 20, label: "گل بهی"},
+  {id: 22, label: "سبز"},
+  {id: 23, label: "کالباسی"},
+  {id: 24, label: "نارنجی"},
+  {id: 25, label: "بنقش"},
+  {id: 26, label: "اسلیپ استوک"},
+  {id: 27, label: "استخونی"}
 ]
+
