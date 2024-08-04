4750525b5abbcbc51ea071e5afedd54bc222194b
hamed <h.arghavan@ompfinex.com>
2024-08-04 17:32:44 +0330

    fix(sale-invoice): share invoice-list component

--- src/app/purchase/component/purchase-receipt/purchase-receipt.component.ts
+++ src/app/purchase/component/purchase-receipt/purchase-receipt.component.ts
@@ -1,15 +1,15 @@
-import {Component, DestroyRef, inject, OnInit} from "@angular/core";
-import {PageContainerComponent} from "@shared/component/page-container/page-container.component";
-import {NzSkeletonModule} from "ng-zorro-antd/skeleton";
-import {PurchaseFacade} from "@purchase/data-access/purchase.facade";
-import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
-import {AsyncPipe} from "@angular/common";
-import {NzEmptyModule} from "ng-zorro-antd/empty";
-import {CardContainerComponent} from "@shared/component/card-container/card-container.component";
-import {PersianDatePipe} from "@sale/pipe/persian-date.pipe";
-import {Product} from "@product/entity/product.entity";
-import {PurchaseItem} from "@purchase/entity/purchase.entity";
-import {IdLabel} from "@shared/entity/common.entity";
+import {Component, DestroyRef, inject, OnInit} from '@angular/core';
+import {PageContainerComponent} from '@shared/component/page-container/page-container.component';
+import {NzSkeletonModule} from 'ng-zorro-antd/skeleton';
+import {PurchaseFacade} from '@purchase/data-access/purchase.facade';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {AsyncPipe} from '@angular/common';
+import {NzEmptyModule} from 'ng-zorro-antd/empty';
+import {CardContainerComponent} from '@shared/component/card-container/card-container.component';
+import {PersianDatePipe} from '@sale/pipe/persian-date.pipe';
+import {Product} from '@product/entity/product.entity';
+import {PurchaseItem} from '@purchase/entity/purchase.entity';
+import {IdLabel} from '@shared/entity/common.entity';
 
 @Component({
   selector: 'purchase-receipt',
@@ -39,14 +39,15 @@ export class PurchaseReceiptComponent implements OnInit {
     return items.reduce((acc, cur, index) => {
       const key = cur.product.code;
       if (!acc.find(item => item.product.code === key)) {
-        acc[index].colors.push(cur.color);
-        acc[index].sizes.push(cur.size);
         acc.push({
           ...cur,
           colors: acc[index].colors,
           sizes: acc[index].sizes,
           product: cur.product
         })
+      } else {
+        acc[index].colors.push(cur.color);
+        acc[index].sizes.push(cur.size);
       }
       return acc;
     }, new Array<PurchaseItem & { product: Product } & { colors: IdLabel[] } & { sizes: IdLabel[] }>())
--- src/app/sale/component/invoice-list/invoice-list.component.ts
+++ src/app/sale/component/invoice-list/invoice-list.component.ts
@@ -1,6 +1,6 @@
-import {Component, DestroyRef, inject, OnInit} from '@angular/core';
+import {Component, DestroyRef, inject, OnInit, signal} from '@angular/core';
 import {SaleFacade} from '@sale/data-access/sale.facade';
-import {AsyncPipe, CurrencyPipe, DecimalPipe, NgIf} from '@angular/common';
+import {AsyncPipe, CurrencyPipe, DecimalPipe, NgIf, NgTemplateOutlet} from '@angular/common';
 import {NzEmptyModule} from 'ng-zorro-antd/empty';
 import {NzSkeletonModule} from 'ng-zorro-antd/skeleton';
 import {FormsModule} from '@angular/forms';
@@ -15,12 +15,13 @@ import {NzButtonModule} from 'ng-zorro-antd/button';
 import {NzModalModule} from 'ng-zorro-antd/modal';
 import {InventoryApi} from '@inventory/api/inventory.api';
 import {Router, RouterLink} from '@angular/router';
+import * as htmlToImage from 'html-to-image';
 
 @Component({
   selector: 'invoice-list',
   standalone: true,
-  imports: [NzModalModule, NzButtonModule, NzPaginationModule, AsyncPipe, NzEmptyModule, CurrencyPipe, NzSkeletonModule, DecimalPipe, FormsModule, PageContainerComponent, CardContainerComponent, PersianDatePipe, CurrencyComponent, NgIf, RouterLink],
-  templateUrl: './invoice-list.component.html',
+  imports: [NzModalModule, NzButtonModule, NzPaginationModule, AsyncPipe, NzEmptyModule, CurrencyPipe, NzSkeletonModule, DecimalPipe, FormsModule, PageContainerComponent, CardContainerComponent, PersianDatePipe, CurrencyComponent, NgIf, RouterLink, NgTemplateOutlet],
+  templateUrl: './invoice-list.component.html'
 })
 export class InvoiceListComponent implements OnInit {
   private readonly saleFacade = inject(SaleFacade);
@@ -29,6 +30,8 @@ export class InvoiceListComponent implements OnInit {
   private readonly inventoryApi = inject(InventoryApi);
   invoiceData: SaleInvoice[] = [];
   loadingState = false;
+  loadingButtons = false;
+  selectedInvoice = signal<SaleInvoice | null>(null);
 
   ngOnInit() {
     this.saleFacade.loadInvoices().then();
@@ -42,7 +45,39 @@ export class InvoiceListComponent implements OnInit {
       });
   }
 
+  async generateReceipt(invoice: SaleInvoice) {
+    this.selectedInvoice.set(invoice);
+    try {
+      this.loadingButtons = true;
+      await new Promise(resolve => setTimeout(resolve, 100));
+      if (window.innerWidth > 768) {
+        const dataUrl = await htmlToImage.toPng(document.getElementById('receipt') as HTMLElement, { quality: 1, style: { visibility: 'visible' } });
+        const link = document.createElement('a');
+        link.href = dataUrl;
+        link.download = `sale-receipt-${invoice.customer.phone}.png`;
+        link.click();
+      } else {
+        const blob = await htmlToImage.toBlob(document.getElementById('receipt') as HTMLElement);
+        if (!blob) {
+          console.error('blob is null');
+          return;
+        }
+        const file = new File([blob], 'receipt.png', {type: 'image/png'});
+        await navigator.share({
+          files: [file],
+          title: `شماره فاکتور`,
+          text: `شماره فاکتور: ${invoice.number}`
+        });
+      }
+    } catch (e) {
+      console.error('error', e);
+    } finally {
+      this.selectedInvoice.set(null);
+      this.loadingButtons = false;
+    }
+  }
+
   navigateToEdit(invoice: SaleInvoice) {
-    this.router.navigate(['/sale'], { state: { invoice: invoice } });
+    this.router.navigate(['/sale'], {state: {invoice: invoice}});
   }
 }
