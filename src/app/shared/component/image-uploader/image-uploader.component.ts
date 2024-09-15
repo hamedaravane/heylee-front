043f775807faf2a16bc0eca18dda9bf382707262
hamed <h.arghavan@ompfinex.com>
2024-09-15 18:21:45 +0330

    fix(breakpoint): fix divider style color & add multiple files feature to image-uploader

--- src/app/shared/component/image-uploader/image-uploader.component.ts
+++ src/app/shared/component/image-uploader/image-uploader.component.ts
@@ -1,49 +1,57 @@
-import {Component, ElementRef, EventEmitter, inject, Input, Output, ViewChild} from "@angular/core";
-import {NzButtonModule} from "ng-zorro-antd/button";
-import {NzMessageService} from "ng-zorro-antd/message";
-import {NzBytesPipe} from "ng-zorro-antd/pipes";
+import {Component, ElementRef, EventEmitter, inject, Input, Output, ViewChild} from '@angular/core';
+import {NzButtonModule} from 'ng-zorro-antd/button';
+import {NzMessageService} from 'ng-zorro-antd/message';
+import {NzBytesPipe} from 'ng-zorro-antd/pipes';
 
 @Component({
   selector: 'image-uploader',
   imports: [NzButtonModule, NzBytesPipe],
   standalone: true,
-  templateUrl: './image-uploader.component.html',
+  templateUrl: './image-uploader.component.html'
 })
 export class ImageUploaderComponent {
+  private readonly nzMessageService = inject(NzMessageService);
   @Input() acceptedFormats: string[] = ['.jpg', '.png', '.jpeg'];
-  @Output() onFileSelected = new EventEmitter<File | null>();
   @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
+  @Input() isMultiple: boolean = false;
+  @Output() onFileSelected = new EventEmitter<File | null>();
+  @Output() onMultipleFileSelected = new EventEmitter<File[] | null>();
   selectedFile: File | null = null;
   selectedFileUrl: string | null = null;
   uploadProgress = 0;
-  private readonly nzMessageService = inject(NzMessageService);
 
-  onClick() {
-    this.fileInput.nativeElement.click();
+  private assignFile(file: File) {
+    if (this.isAcceptedFormats(file)) {
+      this.startProgress();
+      this.selectedFile = file;
+      this.onFileSelected.emit(file);
+      this.selectedFileUrl = URL.createObjectURL(this.selectedFile);
+    } else this.nzMessageService.error('فرمت فایل درست نیست');
   }
 
-  onDragOver(event: DragEvent) {
-    event.preventDefault();
+  private assignMultipleFiles(files: FileList) {
+    if (this.isAcceptedFormats(files)) {
+      this.onMultipleFileSelected.emit(this.extractFileOfFileList(files));
+    } else this.nzMessageService.error('فرمت فایل ها درست نیست');
   }
 
-  onDrop(event: DragEvent) {
-    event.preventDefault();
-    if (event.dataTransfer && event.dataTransfer.files.length > 0) {
-      this.assignFile(event.dataTransfer.files[0]);
+  private extractFileOfFileList(fileList: FileList) {
+    const fileArray: File[] = [];
+    for (let i = 0; i < fileList.length; i++) {
+      if (fileList.item(i)) {
+        fileArray.push(fileList.item(i)!);
+      }
     }
+    return fileArray;
   }
 
-  onFileChange(event: Event) {
-    const input = event.target as HTMLInputElement;
-    if (input.files && input.files.length > 0) {
-      this.assignFile(input.files[0]);
+  private isAcceptedFormats(file: File | FileList) {
+    if (file instanceof FileList) {
+      const files = this.extractFileOfFileList(file);
+      return files.filter(f => this.acceptedFormats.some(format => f.name.endsWith(format))).length === file.length;
+    } else {
+      return this.acceptedFormats.some(format => file.name.endsWith(format));
     }
-  }
-
-  removeFile() {
-    this.selectedFile = null;
-    this.selectedFileUrl = null;
-    this.onFileSelected.emit(null);
   }
 
   private startProgress() {
@@ -56,14 +64,43 @@ export class ImageUploaderComponent {
     }, 20);
   }
 
-  private assignFile(file: File) {
-    if (this.acceptedFormats.some(format => file.name.endsWith(format))) {
-      this.startProgress();
-      this.selectedFile = file;
-      this.onFileSelected.emit(file);
-      this.selectedFileUrl = URL.createObjectURL(this.selectedFile);
-    } else {
-      this.nzMessageService.error('فرمت فایل نامعتبر است');
+  onClick() {
+    this.fileInput.nativeElement.click();
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    if (this.isMultiple) {
+      const files = event.dataTransfer?.files;
+      if (files) {
+        this.assignMultipleFiles(files);
+      }
     }
+    if (event.dataTransfer && event.dataTransfer.files.length > 0) {
+      this.assignFile(event.dataTransfer.files[0]);
+    }
+  }
+
+  onFileChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (this.isMultiple) {
+      const files = input.files;
+      if (files) {
+        this.assignMultipleFiles(files);
+      }
+    }
+    if (input.files && input.files.length > 0) {
+      this.assignFile(input.files[0]);
+    }
+  }
+
+  removeFile() {
+    this.selectedFile = null;
+    this.selectedFileUrl = null;
+    this.onFileSelected.emit(null);
   }
 }
